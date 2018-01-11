a72f7764cb1ac20658e94b34243e2edc330261b3
Chris Baker <chris.l.baker@gmail.com>
2018-01-10 21:48:40 -0500

    update.sh: use tput instead of hardcoded escapes

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 
 [ -z "$DIFF" ] && DIFF="/usr/bin/diff"
-[ -z "$BASH" ] || shopt -s xpg_echo
+
+BOLD="$(tput bold)"
+OFFBOLD="$(tput sgr0)"
 
 access_test ()
 {
@@ -11,23 +13,23 @@ access_test ()
 compare_files ()
 {
 	access_test "$2" || {
-		echo "Unable to access \033[1m$2\033[0m"
+		echo "Unable to access ${BOLD}$2${OFFBOLD}"
 		return 1
 	}
 
 	/usr/bin/cmp -s "$1" "$2" || {
-		echo "Files \033[1m$1\033[0m and \033[1m$2\033[0m do not match."
+		echo "Files ${BOLD}$1${OFFBOLD} and ${BOLD}$2${OFFBOLD} do not match."
 		while :; do
 			read -p "[V]iew [C]opy [S]kip ? " i
 			case $i in
 			v*|V*)
-				echo "Comparing \033[1m$2\033[0m to \033[1m$1\033[0m"
+				echo "Comparing ${BOLD}$2${OFFBOLD} to ${BOLD}$1${OFFBOLD}"
 				"$DIFF" "$2" "$1"
 				compare_files "$1" "$2"
 				break
 				;;
 			c*|C*)
-				echo "Copying \033[1m$2\033[0m to \033[1m$1\033[0m"
+				echo "Copying ${BOLD}$2${OFFBOLD} to ${BOLD}$1${OFFBOLD}"
 				/usr/bin/cp "$2" "$1"
 				return 0
 				;;
@@ -84,7 +86,7 @@ script_main ()
 	echo "  $matches are identical"
 	[ $skips -eq 0 ] \
 		&& echo "  none are different" \
-		|| echo "  \033[1m$skips skipped\033[0m"
+		|| echo "  ${BOLD}$skips skipped${OFFBOLD}"
 }
 
 script_main "$@"
