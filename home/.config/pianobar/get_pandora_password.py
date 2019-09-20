c0c124dddcbae537f43151b950454b09986eb6fc
Chris Baker <chris.l.baker@gmail.com>
2019-09-20 17:01:01 -0400

    pianobar: add ability to unlock keyring

--- home/.config/pianobar/get_pandora_password.py
+++ home/.config/pianobar/get_pandora_password.py
@@ -9,11 +9,19 @@ def eprint(*args, **kwargs):
 dbus = secretstorage.dbus_init()
 logins = secretstorage.get_default_collection(dbus)
 
+if (logins.is_locked()):
+	eprint("Keyring locked, requesting unlock");
+	if (logins.unlock()):
+		eprint("Unlock operation failed, aborting")
+		sys.exit(1)
+
 eprint("Searching %s keyring for Pandora password" % logins.get_label());
 for i in logins.get_all_items():
 	if i.get_label().lower() == "pandora":
 		eprint("Found Pandora")
 		print(i.get_secret().decode())
-		break
+		sys.exit(0);
 	else:
 		eprint("Found something else")
+
+sys.exit(1)
